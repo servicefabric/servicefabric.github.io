e916e0545d284f226930e046a15bf3217cfa89a5
ronen <ronen.hamias@gmail.com>
2019-03-07 16:38:30 +0200

    check style

--- plotly/components/Chart.js
+++ plotly/components/Chart.js
@@ -1,18 +1,31 @@
 var index = 0;
 
-function createElement (containerId) {
+function createElement(containerId) {
     var containersDiv = document.getElementById(containerId);
     // create a new div element
     var newDiv = document.createElement("div");
     newDiv.id = "chart-container" + index++;
     containersDiv.appendChild(newDiv);
     return newDiv.id;
-};
+}
+
+function param(variable) {
+    var query = window.location.search.substring(1);
+    var vars = query.split('&');
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split('=');
+        if (decodeURIComponent(pair[0]) == variable) {
+            return decodeURIComponent(pair[1]);
+        }
+    }
+    console.log('Query variable %s not found', variable);
+    return undefined;
+}
 
 function getJSON(url, callback) {
     var xhr = new XMLHttpRequest();
-    xhr.open('GET', url, true);
-    xhr.responseType = 'text';
+    xhr.open("GET", url, true);
+    xhr.responseType = "text";
     xhr.onload = function () {
         var status = xhr.status;
         if (status === 200) {
@@ -22,7 +35,7 @@ function getJSON(url, callback) {
         }
     };
     xhr.send();
-};
+}
 
 function importJson(url) {
     return new Promise((resolve, reject) => {
@@ -30,13 +43,13 @@ function importJson(url) {
                 if (status === null) {
                     resolve(model);
                 } else {
-                    let err = new Error('error fetchin json from url: ');
+                    let err = new Error("error fetchin json from url: ");
                     reject(err); // reject
                 }
             });
         }
-    )
-};
+    );
+}
 
 
 function fetchTheme(url) {
@@ -48,31 +61,38 @@ function fetchTraces(links) {
     let count = links.length;
 
     return new Promise((resolve, reject) => {
-            links.forEach((element) => {
-                console.log("import: " + element);
-                importJson(element).then(model => {
-                    console.log(model);
-                    traces.push(model);
-                    if ((count = count - 1) <= 0) {
-                        resolve(traces);
-                    }
-                }, err => {
-                    if ((count = count - 1) <= 0) {
-                        resolve(traces);
-                    }
-                });
+        links.forEach((element) => {
+            console.log("import: " + element);
+            importJson(element).then(model => {
+                console.log(model);
+                traces.push(model);
+                if ((count = count - 1) <= 0) {
+                    resolve(traces);
+                }
+            }, err => {
+                if ((count = count - 1) <= 0) {
+                    resolve(traces);
+                }
             });
         });
+    });
 }
 
 function createChart(containerId, url) {
-    importJson(url).then(charts=> {
+    if (!url) {
+        if (param('url')) {
+            url = param('url');
+        } else {
+            console.log("url param is not provided and not found in query string");
+        }
+    }
+    importJson(url).then(charts => {
         fetchTheme("../components/DarkTheme.json").then((layout1) => {
-            charts.forEach(c =>{
-                layout1.title.text = c.chartTitleText;
-                layout1.xaxis.title.text = c.xAxisText;
-                layout1.yaxis.title.text = c.yAxisText;
-                new LineChart(createElement(containerId), fetchTraces(c.traces), layout1);
+            charts.forEach(chart => {
+                layout1.title.text = chart.chartTitleText;
+                layout1.xaxis.title.text = chart.xAxisText;
+                layout1.yaxis.title.text = chart.yAxisText;
+                new LineChart(createElement(containerId), fetchTraces(chart.traces), layout1);
             });
         });
     });
