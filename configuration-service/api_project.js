06f8f5f2b2c7629178a0cb7ff8896ea8622a92fe
io-scalecube-ci <io.scalecube.ci@gmail.com>
2019-05-06 07:03:31 +0000

    Feature: updated configuration-service documentation

--- configuration-service/api_project.js
+++ configuration-service/api_project.js
@@ -1,6 +1,6 @@
 define({
   "name": "Configuration service",
-  "version": "2.1.5-SNAPSHOT",
+  "version": "2.1.6-SNAPSHOT",
   "description": "Configuration service API",
   "title": "API documentation for the Configuration Service",
   "template": {
@@ -18,7 +18,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2019-04-22T06:34:46.485Z",
+    "time": "2019-05-06T07:03:28.017Z",
     "url": "http://apidocjs.com",
     "version": "0.17.7"
   }
